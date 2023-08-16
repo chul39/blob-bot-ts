2dc9379526719130564892b5cb0d7568a8f74f06
Chunlaphat Suwannaat <c.suwannaat@gmail.com>
2023-08-16 22:31:10 +0900

    Fix issues with Azure App Service

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,19 @@ import { DiscordClient } from './discord-client';
 dotenv.config();
 
 /**
+ * Listening to a specific port (should be 8080)
+ * to make sure the bot works on Azure App Service
+ */
+import http from 'http';
+const server = http.createServer((req, res)=>{
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('This is the server for blob-bot-discord-ts.');
+});
+const port = process.env.PORT || '3000';
+server.listen(port);
+
+/**
  * Initailize discord bot client
  */
 const client = new DiscordClient();
